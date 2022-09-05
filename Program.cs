dda2f98b785f00161bcbbc2c1aa01cfa51e6201e
Danil <Kutfilloev.danil@yandex.ru>
2022-09-05 22:10:00 +0500

    оказалось сложно

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+void PrintNumbers(int m, int n)
+{
+    if(m > n)
+    return;
+    Console.Write($"{m} ");
+    PrintNumbers(m+1, n);
+}
+PrintNumbers(1, 5);
+
+
+
+// // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        Console.Write($"{m} ");
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+Console.WriteLine(SumMN(1, 10));
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+Console.WriteLine(Ack(2, 3));
